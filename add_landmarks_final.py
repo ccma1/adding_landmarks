15cd28a175dfafd046d49801a24ecac8ac2e013e
ccma1 <79416075+ccma1@users.noreply.github.com>
2023-06-12 14:43:55 -0400

    Updated code

--- add_landmarks_final.py
+++ add_landmarks_final.py
@@ -3,7 +3,8 @@
 For a given gaze file, creates a new Pandas Dataframe with the following columns:
  - gnl_timestamp - from the MediaPipe Json
  - video_timestamp - from the MediaPipe Json
- - gaze_timestamp - from the MediaPipe Json
+ - gaze_x - from the MediaPipe Json
+ - gaze_y - from the MediaPipe Json
  - closest_point_index - index of the closest point from the array of facemesh points from the MediaPipe Json
       - None if there is no gaze point or facemesh point
  - closest_point_x - x coordinate of the closest facemesh point
@@ -12,15 +13,18 @@ For a given gaze file, creates a new Pandas Dataframe with the following columns
       - nan if there is no gaze point or facemesh point
  - closest_point_distance - distance of the gaze_xy point from the closest point
       - nan if there is no gaze point or facemesh point
- - on_face - 0 if gaze and facemesh is present but gaze isn't inside the face
+ - gaze_on_face - 0 if gaze and facemesh is present but gaze isn't inside the face
       - 1 if gaze is inside the facemesh within a certain threshold
       - 2 if gaze isn't present or facemesh points aren't present
- - distance_to_face - distance of gaze to facemesh
+ - gaze_on_20_buffer - same as gaze_on_face except for 20% buffer defined below
+ - gaze_on_30_buffer - same as gaze_on_face except for 30% buffer defined below
+ - gaze_to_face_distance - distance of gaze to facemesh
       - nan if there is no gaze point or facemesh point
- - on_facial_feature - if gaze point is inside polygon of facial feature.
-      - one of {left_eye, right_eye, nose, mouth, none}
- - closest_facial_feature - the closest facial feature to the gaze point
-      - one of {left_eye, right_eye, nose, mouth}
+ - gaze_to_20_buffer_distance - same as gaze_to_face_distance except for 20% buffer
+ - gaze_to_30_buffer_distance - same as gaze_to_face_distance except for 30% buffer
+ - on_facial_feature - 'nan' if there is no gaze_x or no gaze_y or no facemesh 
+      - or combination of {0: none, 1: left_eye, 2:right_eye, 3:nose, 4:mouth}, eg. 13 := left_eye and nose
+ - closest_facial_feature - same as on_facial_feature
  - distance_to_left_eye - distance to left eye
       - nan if there is no gaze point or facemesh point
  - distance_to_right_eye - distance to right eye
@@ -48,10 +52,33 @@ from typing import Tuple, List
 from shapely import Point, Polygon, MultiPoint
 from operator import itemgetter
 
-ON_FACE_DISTANCE_THRESHOLD = 0
+"""
+This is fraction of the max(width, height) of the bounding rectangle of the convex hull.
+See: https://shapely.readthedocs.io/en/stable/reference/shapely.Polygon.html#shapely.Polygon.minimum_rotated_rectangle
 
-FACIAL_FEATURE_NAMES = ["left_eye", "right_eye", "nose", "mouth", "none"]
+A new column will be created for each threshold.
 
+json takes format: 
+{
+   "frame_x": {
+      "gnl_timestamp": "timestamp",
+      "video_timestamp": "timestamp",
+      "gaze_x": "",
+      "gaze_y": "",
+      "Body [x, y, visibility, presence]":"",
+      "Face [x, y, visibility, presence]":"",
+      "Hands [x, y, visibility, presence]": ""
+   }
+}
+
+"""
+
+# We use 20% and 30% of the max(width, height) where width and height are of the face convex hull.
+ON_FACE_DISTANCE_THRESHOLD = [0.2, 0.3]
+
+FACIAL_FEATURE_NAMES = ["nan", "left_eye", "right_eye", "nose", "mouth"]
+
+# Not used for reasons described in readme
 FACE_SILHOUETTE = [
     10,  338, 297, 332, 284, 251, 389, 356, 454, 323, 361, 288,
     397, 365, 379, 378, 400, 377, 152, 148, 176, 149, 150, 136,
@@ -66,59 +93,84 @@ NOSE_SILHOUETTE = [8, 351, 399, 420, 429, 358, 327, 326, 2, 97, 98, 129, 209, 19
 
 MOUTH_SILHOUETTE = [164, 393, 391, 322, 410, 432, 273, 335, 406, 313, 18, 83, 182, 106, 43, 212, 186, 92, 165, 167, 164]
 
-NON_FEATURE_POINTS = [9, 10, 21, 32, 34, 36, 47, 50, 54, 58, 67, 68, 69, 71, 93, 100, 101, 103, 104, 108, 109, 114, 116, 123, 126, 127, 132, 135, 136, 137, 138, 139, 140, 142, 147, 148, 149, 150, 151, 152, 162, 169, 170, 171, 172, 175, 176, 177, 187, 188, 192, 194, 199, 200, 201, 202, 203, 204, 205, 206, 207, 208, 210, 211, 213, 214, 215, 216, 217, 227, 234, 251, 262, 264, 266, 277, 280, 284, 288, 297, 298, 299, 301, 323, 329, 330, 332, 333, 337, 338, 343, 345, 352, 355, 356, 361, 364, 365, 366, 367, 368, 369, 371, 376, 377, 378, 379, 389, 394, 395, 396, 397, 400, 401, 411, 412, 416, 418, 421, 422, 423, 424, 425, 426, 427, 428, 430, 431, 433, 434, 435, 436, 437, 447, 454]
-
 def GetClosestPoint(gaze_point: shapely.Point, face_points: List):
    """
    Returns (index of point, (point_x, point_y), distance of gaze to point)
+   We need to check all the points. See Readme for edge cases that show why.
    """
-   closest_point = functools.reduce(
-        lambda p1, p2: p1 if math.dist((gaze_point.x, gaze_point.y),p1[1]) <= math.dist((gaze_point.x, gaze_point.y),p2[1]) else p2,
-        list(enumerate(face_points))
-    )
-   
-   return (closest_point[0], closest_point[1], math.dist((gaze_point.x, gaze_point.y), closest_point[1]))
+   index, (closest_point, dist) = functools.reduce(
+      lambda p1, p2: p1 if p1[1][1] < p2[1][1] else p2,
+      enumerate(map(lambda point: (point, math.dist(point, (gaze_point.x, gaze_point.y))), face_points))
+   )
+
+   return (index, closest_point, dist)
 
 def GetClosestFeature(gaze_point: shapely.Point, distance_to_face: float, facial_polygons: List[shapely.Polygon]):
    """
+   distance_to_face is the distance to the convex hull that defines the face.
+
    Returns (
-      index of facial feature gaze is on, one of {0:left_eye, 1: right_eye, 2:nose, 3:mouth, 4:none},
-      index of closest facial feature, one of {0:left_eye, 1: right_eye, 2:nose, 3:mouth, 4:none}, 
-      distance_to_{LEFT_EYE, RIGHT_EYE, NOSE, MOUTH}
+      a combination of {0:none, 1: left_eye, 2:right_eye, 3:nose, 4:mouth}, eg. 13 := left_eye and nose
+      a combination index of closest facial feature, one of {0:none, 1:left_eye, 2: right_eye, 3:nose, 4:mouth}, 
+      distance_to_{LEFT_EYE, RIGHT_EYE, NOSE, MOUTH}, potentially equidistance features
    )
    """
-   on_facial_feature, closest_facial_feature, closest_feat_dist = 4, None, None
+   on_facial_feature, closest_facial_feature, closest_feat_dist = 0, 0, np.inf
    feature_distances = [0, 0, 0, 0]
    for i in range(4):
       feature_poly = facial_polygons[i]
       feature_distances[i] = feature_poly.distance(gaze_point)
 
-      if (distance_to_face == 0 and feature_poly.contains(gaze_point)):
-         on_facial_feature, closest_facial_feature, closest_feat_dist = i, i, 0
-         feature_distances[i] = 0
-      elif closest_feat_dist == None or feature_distances[i] < closest_feat_dist:
-            closest_facial_feature, closest_feat_dist = i, 0
+      if (distance_to_face == 0 and feature_poly.covers(gaze_point)):
+         closest_facial_feature = i + 1 if on_facial_feature == 0 else closest_facial_feature * 10 + i + 1
+         on_facial_feature      = on_facial_feature * 10 + i + 1
+         closest_feat_dist      = 0
+         feature_distances[i]   = 0
+      elif feature_distances[i] == closest_feat_dist:
+         # Feature poly does not cover face point bc. If not on face then not in feature
+         closest_facial_feature = closest_facial_feature * 10 + i + 1
+      elif feature_distances[i] < closest_feat_dist:
+         closest_facial_feature = i + 1
+         closest_feat_dist = feature_distances[i]
    
    return [on_facial_feature, 
            closest_facial_feature,
-           feature_distances[0],
-           feature_distances[1],
-           feature_distances[2],
-           feature_distances[3]]
+           *feature_distances]
 
 def GazeOnFace(gaze_point: shapely.Point, face_poly: shapely.Polygon): 
    """
+   face_poly is a convex hull of the face.
+
    Returns Tuple (
-      1 if gaze point is within ON_FACE_DISTANCE_THRESHOLD or enclosed by 
-      face polygon to face shape, or 0 otherwise,
-      distance of gaze point to face polygon
+      1 if gaze point is enclosed by face convex hull to face shape, or 0 otherwise,
+      1 if gaze point is within the 20% buffer or 0 otherwise,
+      1 if gaze point is within the 30% buffer or 0 other, 
+      distance of gaze point to face convex hull,
+      distance of gaze point to 20% buffer (just max(0, distance to face convex hull - 20% diameter)),
+      distance of gaze point to 30% buffer (just max(0, distance to face convex hull - 20% diameter))
    )
    """  
-   if face_poly.contains(gaze_point):
-      return (1, 0)
+   box_xx, box_yy = face_poly.minimum_rotated_rectangle.exterior.coords.xy
+   box_points = list(zip(box_xx, box_yy))
+   box_width = math.dist(box_points[1],box_points[2])
+   box_height = math.dist(box_points[0],box_points[1])
+   face_hull_diameter = max(box_width, box_height)
+
+   if gaze_point.covered_by(face_poly):
+      # Inside the face convex hull
+      return (1, 1, 1, 0, 0, 0)
    
+   # Shortest distance from gaze point to convex hull of face
    gaze_dist = face_poly.distance(gaze_point)
-   return (1 if gaze_dist <= ON_FACE_DISTANCE_THRESHOLD else 0, gaze_dist)
+   if ((buff_one_dist := gaze_dist - ON_FACE_DISTANCE_THRESHOLD[0] * face_hull_diameter) <= 0):
+      # Inside the 20% Buffer
+      return (0, 1, 1, gaze_dist, 0, 0)
+   elif ((buff_two_dist := gaze_dist - ON_FACE_DISTANCE_THRESHOLD[1] * face_hull_diameter) <= 0):
+      # Inside the 30% buffer
+      return (0, 0, 1, gaze_dist, buff_one_dist, 0)
+   
+   # Definitely outside
+   return (0, 0, 0, gaze_dist, buff_one_dist, buff_two_dist)
 
 def CreatePolygons(face_points: List[Tuple[float, float]]):
    """
@@ -144,39 +196,38 @@ def GetGazeFacialFeatureData(gaze_xy: shapely.Point, face_points: List):
    
    Returns
    -------
-   [closest_point_index, 
-    closest_point: (x, y), 
-    gaze_on_face, 
-    distance_to_face,
-    on_facial_feature,  # {left_eye, right_eye, nose, mouth, none}
-    closest_facial_feature, # {left_eye, right_eye, nose, mouth}
-    left_eye_distance,
-    right_eye_distance,
-    nose_distance,
-    mouth_distance]
+   [
+      # 0-3: Closest point info
+      closest_point_index, 
+      closest_point_x,
+      closest_point_y,
+      closest_point_distance,
+      # 4-9: Gaze on face info
+      1 if gaze point is enclosed by face convex hull to face shape, or 0 otherwise,
+      1 if gaze point is within the 20% buffer or 0 otherwise,
+      1 if gaze point is within the 30% buffer or 0 other, 
+      distance of gaze point to face convex hull,
+      distance of gaze point to 20% buffer (just max(0, distance to face convex hull - 20% diameter)),
+      distance of gaze point to 30% buffer (just max(0, distance to face convex hull - 20% diameter)),
+      # 10-15 Gaze on facial feature info
+      a combination of {0:none, 1: left_eye, 2:right_eye, 3:nose, 4:mouth},
+      a combination index of closest facial feature, one of {0:none, 1:left_eye, 2: right_eye, 3:nose, 4:mouth},
+      distance_to_{LEFT_EYE, RIGHT_EYE, NOSE, MOUTH}
+   ]
    """
    facial_polygons                = CreatePolygons(face_points)
-   gaze_on_face, distance_to_face = GazeOnFace(gaze_xy, facial_polygons[0])
-   facial_feature_data            = GetClosestFeature(gaze_xy, distance_to_face, facial_polygons[1:])
-   if gaze_on_face and facial_feature_data[0] == 4:
-      # Gaze on face but not on any facial feature
-      closest_point_index, closest_point, closest_point_dist = GetClosestPoint(gaze_xy, face_points)
-   elif gaze_on_face and facial_feature_data[0] != 4:
-      # Gaze on face and on facial feature
-      closest_point_index, closest_point, closest_point_dist = GetClosestPoint(gaze_xy, face_points)
-   else:
-      # Gaze not on face
-      closest_point_index, closest_point, closest_point_dist = GetClosestPoint(gaze_xy, face_points)
+   gaze_on_face_data              = GazeOnFace(gaze_xy, facial_polygons[0])
+   facial_feature_data            = GetClosestFeature(gaze_xy, gaze_on_face_data[3], facial_polygons[1:])
+   closest_point_index, closest_point, closest_point_dist = GetClosestPoint(gaze_xy, face_points)
+
    return [
       closest_point_index, 
-      closest_point[0], 
-      closest_point[1], 
+      closest_point[0], # x-coordinate
+      closest_point[1], # y-coordinate
       closest_point_dist, 
-      gaze_on_face, 
-      distance_to_face,
-      FACIAL_FEATURE_NAMES[facial_feature_data[0]], # Mapping to naps
-      FACIAL_FEATURE_NAMES[facial_feature_data[1]]
-   ] + facial_feature_data[2:]
+      *gaze_on_face_data,
+      *facial_feature_data
+   ] 
 
 def AddLandmarkInfo(mediapipe_file: str) -> pd.DataFrame:
    """
@@ -202,15 +253,20 @@ def AddLandmarkInfo(mediapipe_file: str) -> pd.DataFrame:
          result.append([
             int(frame['gnl_timestamp']), 
             int(frame['video_timestamp']),
-            int(frame['gaze_timestamp']),
-            None,         # Index of closest face point 
+            frame['gaze_x'],
+            frame['gaze_y'],
+            np.nan,       # Index of closest face point 
             float('nan'), # Closest face point x
             float('nan'), # Closest face point y
             float('nan'), # Distance of gaze to closest face point x, y
-            2,            # On face
+            2,            # On face convex hull
+            2,            # On 20% buffer 
+            2,            # On 30% buffer 
             float('nan'), # Distance to face 
-            'none',       # On facial feature
-            'none',       # Closest facial feature
+            float('nan'), # Distance to face 20%  
+            float('nan'), # Distance to face 30% 
+            'nan',        # On facial feature
+            'nan',        # Closest facial feature
             float('nan'), # Distance to left_eye
             float('nan'), # Distance to right_eye
             float('nan'), # Distance to nose
@@ -223,21 +279,26 @@ def AddLandmarkInfo(mediapipe_file: str) -> pd.DataFrame:
             for lm in frame['Face [x, y, visibility, presence]'].split()
          ]
          result.append(
-            [int(frame['gnl_timestamp']), int(frame['video_timestamp']),int(frame['gaze_timestamp'])] + 
+            [int(frame['gnl_timestamp']), int(frame['video_timestamp']), frame['gaze_x'], frame['gaze_y']] + 
             GetGazeFacialFeatureData(gaze_xy, face_lmks)
          )
          del face_lmks
 
    df = pd.DataFrame(result)
    df.columns = ['gnl_timestamp', 
-                 'video_timestamp', 
-                 'gaze_timestamp', 
+                 'video_timestamp',
+                 'gaze_x',
+                 'gaze_y',
                  'closest_point_index', 
                  'closest_point_x', 
                  'closest_point_y', 
                  'closest_point_distance', 
                  'gaze_on_face',
+                 'gaze_on_20_buffer',
+                 'gaze_on_30_buffer',
                  'gaze_to_face_distance',
+                 'gaze_to_20_buffer_distance',
+                 'gaze_to_30_buffer_distance',
                  'on_facial_feature',
                  'closest_facial_feature', 
                  'gaze_to_left_eye_distance',
@@ -252,18 +313,28 @@ def AddLandmarkInfo(mediapipe_file: str) -> pd.DataFrame:
 def main():
    # res = AddLandmarkInfo("./data/test_data.json")
    # res = AddLandmarkInfo("./data/20Mar_S1_g2_Data.json")
-   res = AddLandmarkInfo("./data/04Apr_S1_g2_Data.json")
-   res.to_csv('test_04Apr_S1_g2_Data.csv', index=False, na_rep='nan')
+   res = AddLandmarkInfo("./data/04Apr_S1_g1_Data.json")
+   res.to_csv('test_04Apr_S1_g1_Data.csv', index=False, na_rep='nan')
 
 if __name__ == "__main__":
    import cProfile as profile
    import pstats
-   prof = profile.Profile()
-   prof.enable()
-   main()
-   prof.disable()
-   # print program output
-   print('Done!')
-   # print profiling output
-   stats = pstats.Stats(prof).strip_dirs().sort_stats("cumtime")
-   stats.print_stats(30) 
+   import time
+   # Profiling code if interested...
+
+   PROFILE = True
+   if PROFILE:
+      # prof = profile.Profile()
+      # prof.enable()
+      # main()
+      # prof.disable()
+      # # print program output
+      # print('Done!')
+      # # print profiling output
+      # stats = pstats.Stats(prof).strip_dirs().sort_stats("cumtime")
+      # stats.print_stats(30) 
+      start = time.time()
+      main()
+      print(time.time() - start)
+   else: 
+      main()
--- gaze_on_landmarks.py
+++ gaze_on_landmarks.py
@@ -4,8 +4,9 @@ Given Alvaro's mediapipe file and gaze file, it will produce a new gaze file.
 The gaze file will have an indicator for if the gaze is on the person's face, 
 body, or eye.
 
-It will also produce a new file with indicators for if both people are looking
-each other's face, body, and are making eye contact. 
+You can also visualize the mediapipe face and body points if desired using the test files.
+
+Read the Readme for how the points are determined and the codes for the points.
 """
 import os
 import pandas as pd
@@ -14,24 +15,28 @@ from add_landmarks_final import AddLandmarkInfo
 
 def GetGazeOnFaceData(mediapipe_file: str, name_to_save: str):
     """
-    Expect around ~40 FPS for processing time.
+    Expect around ~80 FPS for processing speed.
+
+    373.96822142601013 seconds for 30596 frames
     """
     g1_gaze_df = AddLandmarkInfo(mediapipe_file)
     g1_gaze_df.to_csv(name_to_save, index=False, na_rep='nan')
 
-def ProcessAll(json_dir: str):
+def ProcessAll(json_dir: str, save_to_dir: str):
     for fname in os.listdir(json_dir):
-        if fname == "20Mar_S1_g_Data.json":
+        if ((fname[:-9]+"face_data.csv") in os.listdir(save_to_dir)):
+            print(f"Skipping {fname}: already processed")
             continue
+        if (os.stat(json_dir + fname).st_size == 0):
+            print(f'Skipping {fname}: file size 0')
+            continue
+
         print(f'Adding Facemesh Data for {fname}')
         GetGazeOnFaceData(json_dir + fname, fname[:-9]+"face_data.csv")
         print(f'Finished, saved to {fname[:-9]+"face_data.csv"}')
 
 if __name__ == "__main__":
-    # g1_mediapipe_file = "/home/Pupilproject/PupilProject/OutputFiles/20Mar_S1_g_Data.json"
-    # g1_face_file = "./results/20Mar_S1_g1_Face_Data.csv"
-
-    # print(f"Adding Facemesh Data for {g1_face_file}")
-    # GetGazeOnFaceData(g1_mediapipe_file, g1_face_file)
-    # print("Finished")
-    ProcessAll("/home/Pupilproject/PupilProject/OutputFiles/")
+    ProcessAll(
+        json_dir="/home/Pupilproject/PupilProject/OutputFiles_new/",
+        save_to_dir="./facemesh_results_new/"
+    )
